84e908583720602dfa6e8f2356586d9465d9fcad
Dan Thanh Ngu <dantn0902@gmail.com>
2025-07-07 19:31:06 +0200

    Update myscripts.js
    add 2 new functions, add and done

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,42 @@
 let extraQ2 = "Well somehow your friends are very convincing and drags you out anyway. Sneaky friends";
 let extraQ3a = "YAS! We were thinking the same thing!";
 let extraQ3b = "naaah we already actually decided. hehe. We were just asking you out of courtsey. put on a cute bikini";
+let mbtiScore = {
+    e: 0,
+    i: 0,
+    n: 0,
+    s: 0,
+    f: 0,
+    t: 0,
+    p: 0,
+    j: 0
+};
 
 function next(previous, next) {
-    var n = document.getElementById(next);
-    var p = document.getElementById(previous);
-    n.style.display = "block";
-    p.style.display = "none";
+    var ne = document.getElementById(next);
+    var pr = document.getElementById(previous);
+    ne.style.display = "block";
+    pr.style.display = "none";
+}
+
+function add(letters){
+    for (let letter of letters){
+        if (mbtiScore.hasOwnProperty(letter)) {
+            mbtiScore[letter]++;
+        }
+    }
+}
+
+function done() {
+    let final = ""
+    let letters = ["e", "i", "n", "s", "f", "t", "j", "p"];
+    for (let i = 0; i < 8; i+=2) {
+        if (mbtiScore[letters[i]] > mbtiScore[letters[i+1]]) {
+            final += letters[i]
+        } else {
+            final += letters[i+1]
+        }
+    }
+    var resultText = document.getElementById("mbti");
+    resultText.textContent = final;
 }
